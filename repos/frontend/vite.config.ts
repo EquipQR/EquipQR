3a80d93e598c9c909b5c51f4b4b8cc120628dffb
Justin N. Pitera <justinpitera@gmail.com>
2025-06-16 12:46:42 +0000

    fix: moved ssl cert and key config to env file for vite

--- repos/frontend/vite.config.ts
+++ repos/frontend/vite.config.ts
@@ -1,17 +1,21 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import fs from 'node:fs';
 import path from 'node:path';
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, './certs/localhost+2-key.pem')),
-      cert: fs.readFileSync(path.resolve(__dirname, './certs/localhost+2.pem')),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react()],
+    server: {
+      https: {
+        key: fs.readFileSync(path.resolve(__dirname, env.VITE_SSL_KEY_PATH)),
+        cert: fs.readFileSync(path.resolve(__dirname, env.VITE_SSL_CERT_PATH)),
+      },
     },
-  },
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+    optimizeDeps: {
+      exclude: ['lucide-react'],
+    },
+  };
 });
